18511b3368b4761c4c77709632a75f0c10d6c0ed
Nancy Priceila <NANCY.B@lntinfotech.com>
2020-12-16 16:48:57 +0530

    CS file added

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter minimum value");
+            int min=Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter maximum value");
+            int max= Convert.ToInt32(Console.ReadLine());
+            bool flag;
+            List<int> l = new List<int>();
+            for(int i = min; i <= max; i++)
+            {
+                flag = true;
+                for(int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag == true && i != 1)
+                {
+                    l.Add(i);
+                }
+            }
+            Console.WriteLine("The prime numbers between {0} and {1} are:", min, max);
+            foreach(int k in l)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
